e0e80ba62eef0d93b4d81c6233d7e37d77f2b841
Brijesh-jethava <brijeshjethva900@gmail.com>
2025-04-19 16:33:21 +0530

    Added cloudinary and update-profile in backend

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {signup,login,logout} from '../controllers/auth.controller.js';
+import {signup,login,logout, checkAuth, updateProfile} from '../controllers/auth.controller.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
 const router = express.Router();
 
@@ -9,4 +9,6 @@ router.post('/logout',logout)
 
 router.put('/update-profile',protectRoute, updateProfile)
 
-export default router;
+router.get('/check',protectRoute, checkAuth)
+
+export default router; 
